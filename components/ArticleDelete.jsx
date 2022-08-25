737680d15fd48948e804742ea209a18abb054404
Hemant <hrsmart2150@gmail.com>
2022-08-26 01:15:29 +0530

    Made whole app responsive

--- components/ArticleDelete.jsx
+++ components/ArticleDelete.jsx
@@ -1,8 +1,8 @@
 import { deleteDoc, doc } from "firebase/firestore";
-import React from "react";
-import { db, storage } from "../firebase";
-import { toast } from "react-toastify";
 import { deleteObject, ref } from "firebase/storage";
+import React from "react";
+import { toast } from "react-toastify";
+import { db, storage } from "../firebase";
 
 
 export default function DeleteArticle({ id, imageUrl }) {
@@ -10,16 +10,16 @@ export default function DeleteArticle({ id, imageUrl }) {
         if (window.confirm("Are you sure you want to delete this article?")) {
             try {
                 await deleteDoc(doc(db, "articles", id));
-                toast("Article deleted successfully", { type: "success" });
+                toast("file deleted successfully", { type: "success" });
                 const storageRef = ref(storage, imageUrl);
                 await deleteObject(storageRef);
             } catch (error) {
-                toast("Error deleting article", { type: "error" });
+                toast("Error in deleting file", { type: "error" });
                 console.log(error);
             }
         }
     };
     return (
-        <button onClick={handleDelete} className="cursor-pointer transition-all duration-300 border-[1.3px] border-red-500 py-2 px-2 rounded-md hover:bg-red-500 hover:text-white  " >Delete</button>
+        <button onClick={handleDelete} className="cursor-pointer transition-all duration-300 border-[1.3px] border-red-500 text-xs sm:text-sm py-[5px] sm:py-2 px-2 rounded-md hover:bg-red-500 hover:text-white ">Delete</button>
     );
 }
--- components/ImageUploader.jsx
+++ components/ImageUploader.jsx
@@ -67,11 +67,11 @@ const ImageUploader = ({ theme }) => {
                         imageUrl: url,
                         createdAt: Timestamp.now().toDate()
                     }).then(() => {
-                        toast("Article added successfully", { type: "success" });
+                        toast("file added successfully", { type: "success" });
                         setProgress(0);
                     })
                         .catch((err) => {
-                            toast("Error adding article", { type: "error" });
+                            toast("Error in adding file", { type: "error" });
                         });
                 })
             });
@@ -86,17 +86,22 @@ const ImageUploader = ({ theme }) => {
     }
 
     return (
-        <div className='grid grid-cols-2  '>
-            <div className="flex col-span-1 h-fit fixed ml-40 mt-20">
-                <div className={`max-w-2xl w-80 rounded-lg shadow-xl ${theme === 'dark' ? 'bg-[#535352]' : 'bg-gray-50'}`}>
+        <div className='flex flex-col sm:flex-row sm:justify-between '>
 
+            {/* image uploader section  */}
+            <div className="flex h-fit sm:w-80 mt-12 mx-auto max-w-2xl">
+                {/* <div className=''> */}
+                <div className={`mx-auto md:w-80 rounded-lg shadow-xl ${theme === 'dark' ? 'bg-[#535352]' : 'bg-gray-50'}`}>
                     <div className="m-4">
+
+                        {/* file upload  */}
                         <div className={`inline-block mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-500'}`}>
                             File Upload
                         </div>
+
+                        {/* title and desc div  */}
                         <div className="flex items-center justify-center w-full group">
-                            <div
-                                className={`flex flex-col w-full h-32 border-4 border-blue-200 border-dashed ${theme === 'dark' ? 'group-hover:bg-gray-500 group-hover:border-gray-200' : 'group-hover:bg-gray-100 group-hover:border-gray-300'}`}>
+                            <div className={`flex flex-col relative w-full h-32 border-4 border-blue-200 border-dashed ${theme === 'dark' ? 'group-hover:bg-gray-500 group-hover:border-gray-200' : 'group-hover:bg-gray-100 group-hover:border-gray-300'}`}>
                                 <div className="flex flex-col items-center justify-center pt-7">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400"
                                         fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -106,27 +111,29 @@ const ImageUploader = ({ theme }) => {
                                     <span className={`pt-1 text-sm tracking-wider ${theme === 'dark' ? 'text-white' : 'text-gray-400'} `}>
                                         Attach a file
                                     </span>
-                                    <input name='image' onChange={(e) => handleImageChange(e)} type="file" className="opacity-0 z-50 absolute w-full h-[120px] " />
+                                    <input className="opacity-0 z-50 absolute w-[230px] md:w-[282px] h-full top-[.5px] " name='image' onChange={(e) => handleImageChange(e)} type="file" />
                                 </div>
                             </div>
                         </div>
                     </div>
 
+                    {/* title  */}
                     <div className="p-2">
                         <label htmlFor="text" className={`leading-7 text-sm ${theme === 'dark' ? 'text-white' : 'text-gray-600'}`}>Title</label>
                         <input onChange={(e) => handleChange(e)} value={formData.title} type="text" id="title" name="title" className={`w-full ${theme === 'dark' ? 'bg-[#535352] text-white ' : 'bg-gray-50 text-gray-700 '} rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`} />
                     </div>
 
+                    {/* description   */}
                     <div className="p-2">
                         <label htmlFor="text" className={`leading-7 text-sm ${theme === 'dark' ? 'text-white' : 'text-gray-600'}`}>Description</label>
                         <textarea onChange={(e) => handleChange(e)} value={formData.description} type="text" id="description" name="description" className={`w-full ${theme === 'dark' ? 'bg-[#535352] text-white' : 'bg-gray-50 text-gray-700'} rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`} />
                     </div>
 
+                    {/* No file selected or file name  */}
                     <div className="flex justify-center flex-col p-2">
                         {formData.image?.name ? <span>{formData.image?.name}</span> : <span>No file selected</span>}
-
+                        {/* upload button with progress bar  */}
                         <button onClick={handlePublish} className="w-full my-2 px-4 py-2 text-white hover:bg-[#5e6ac0] bg-[#3f51d8] rounded shadow-xl">
-
                             {progress > 0 ? <span>
                                 <Progress
                                     mt="sm"
@@ -138,40 +145,46 @@ const ImageUploader = ({ theme }) => {
                                 />
                             </span>
                                 : 'Upload'}
-
                         </button>
-
                     </div>
 
                 </div>
+                {/* </div> */}
             </div>
 
+            <div className='mx-auto mt-[24px]'>
+                {articles.length > 0 ? <p className={` text-xl pl-4 py-4 font-bold tracking-wider ${theme === 'dark' ? 'text-white' : 'text-gray-700'} `}>All Uploads</p> : <p className={` text-xl pl-4 py-4 font-bold tracking-wider ${theme === 'dark' ? 'text-white' : 'text-gray-700'} `}>Please Upload any image to see here</p>}
+                <div className='px-2 h-[480px] sm:h-[540px] overflow-y-scroll scrollbar-thin scrollbar-thumb-black scroll-smooth'>
+                    <div className='h-fit'>
+                        {articles.map(({ id, title, description, imageUrl, createdAt }) => {
+                            return <div key={id} className="px-2">
+                                <div className={`rounded-lg shadow-lg w-[280px] md:w-[400px] lg:w-[550px] mb-5 overflow-hidden border h-fit ${theme === 'dark' ? 'bg-[#1F262E]' : 'bg-white'} md:mx-0 lg:mx-0`}>
+                                    {/* post image  */}
+                                    <img className="w-full bg-cover h-auto" src={imageUrl} />
 
-            {articles.length > 0 && <p className={`fixed right-[690px] text-xl py-8 top-[80px] font-bold ${theme === 'dark' ? 'text-white' : 'text-gray-700'} tracking-wider`}>Your Uploads</p>}
+                                    <div className="px-3 pb-2">
+                                        {/* post title  */}
+                                        <div className="my-2 text-sm sm:text-lg">
+                                            <span className="font-medium mr-2">{truncateString(title, 30)}</span>
+                                        </div>
 
-            <div className='mt-32 col-span-1 left-[700px] fixed px-5 py-2 overflow-y-scroll scrollbar-thin scrollbar-thumb-black h-[72vh] top-8 scroll-smooth w-fit'>
-                <div className=' h-fit '>
-                    {articles.map(({ id, title, description, imageUrl, createdAt }) => {
-                        return <div key={id} className={`rounded-lg shadow-lg mb-5 overflow-hidden border h-fit w-[500px]  ${theme === 'dark' ? 'bg-[#1F262E]' : 'bg-white'} md:mx-0 lg:mx-0`}>
-                            <img className="w-full bg-cover w-[700px] h-[250px]" src={imageUrl} />
-                            <div className="px-3 pb-2">
-                                <div className="pt-1">
-                                    <div className="mb-2 text-sm">
-                                        <span className="font-medium mr-2">{truncateString(title, 30)}</span>
-                                    </div>
-                                </div>
-                                <div className="text-sm mb-2 text-gray-400 cursor-pointer flex justify-between font-medium">
-                                    <span>{createdAt.toDate().toDateString()}</span>
-                                    <span className='text-red-500 cursor-pointer' ><ArticleDelete id={id} imageUrl={imageUrl} /></span>
-                                </div>
-                                <div className="mb-2">
-                                    <div className="mb-2 text-sm">
-                                        {truncateString(description, 60)}
+                                        {/* delete component  */}
+                                        <div className="py-1 text-gray-400 cursor-pointer flex items-center justify-between font-medium">
+                                            <span className='text-xs sm:text-sm'>{createdAt.toDate().toDateString()}</span>
+                                            <span className='text-red-500 cursor-pointer' ><ArticleDelete id={id} imageUrl={imageUrl} /></span>
+                                        </div>
+
+                                        {/* post description  */}
+                                        <div className="mb-2">
+                                            <div className="mb-2 text-xs sm:text-sm">
+                                                {truncateString(description, 60)}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    })}
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
